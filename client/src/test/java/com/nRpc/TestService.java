b441c9169486100ff504873d4c08d17668b741d2
刘世凯 <shikai.liu@corp.elong.com>
2018-02-08 11:29:50 +0800

    Third commit

---
+++ client/src/test/java/com/nRpc/TestService.java
@@ -0,0 +1,24 @@
+package com.nRpc;
+
+import com.nRpc.client.Annotaion.NRPCService;
+
+/**
+ *
+ *
+ *
+ *
+ *
+ * <p>
+ * 修改历史:                                                                                    &lt;br&gt;
+ * 修改日期             修改人员        版本                     修改内容
+ * --------------------------------------------------------------------
+ * 2018年02月08日 上午11:27   shikai.liu     1.0   初始化创建
+ * </p>
+ *
+ * @author shikai.liu
+ * @version 1.0
+ * @since JDK1.7
+ */
+@NRPCService
+public interface TestService {
+}
