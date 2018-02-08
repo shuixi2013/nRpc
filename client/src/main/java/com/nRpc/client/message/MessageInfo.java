6bc0fb1a75ba3fe4b6a94620cdee5213c331b292
刘世凯 <shikai.liu@corp.elong.com>
2018-02-08 11:22:45 +0800

    Third commit

--- client/src/main/java/com/nRpc/client/message/MessageInfo.java
+++ client/src/main/java/com/nRpc/client/message/MessageInfo.java
@@ -1,4 +1,4 @@
-package com.lskRpc.client;
+package com.nRpc.client.message;
 
 /**
  *
@@ -10,12 +10,12 @@ package com.lskRpc.client;
  * 修改历史:                                                                                    &lt;br&gt;
  * 修改日期             修改人员        版本                     修改内容
  * --------------------------------------------------------------------
- * 2018年02月08日 上午10:25   shikai.liu     1.0   初始化创建
+ * 2018年02月08日 上午10:33   shikai.liu     1.0   初始化创建
  * </p>
  *
  * @author shikai.liu
  * @version 1.0
  * @since JDK1.7
  */
-public class A {
+public class MessageInfo {
 }
