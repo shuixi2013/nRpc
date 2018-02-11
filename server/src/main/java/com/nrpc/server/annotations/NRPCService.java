9904f056e1705e3b3f34754cf7a4259969cf907d
刘世凯 <shikai.liu@corp.elong.com>
2018-02-11 15:49:42 +0800

    修改了pom的依赖

---
+++ server/src/main/java/com/nrpc/server/annotations/NRPCService.java
@@ -0,0 +1,31 @@
+package com.nrpc.server.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ *
+ *
+ *
+ *
+ * <p>
+ * 修改历史:                                                                                    &lt;br&gt;
+ * 修改日期             修改人员        版本                     修改内容
+ * --------------------------------------------------------------------
+ * 2018年02月09日 下午4:57   shikai.liu     1.0   初始化创建
+ * </p>
+ *
+ * @author shikai.liu
+ * @version 1.0
+ * @since JDK1.7
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface NRPCService {
+
+
+
+}
